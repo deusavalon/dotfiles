c3550af5af2cc9e2613e244b0f140efc538237d7
denis <denis.heinen@hotmail.de>
2019-11-07 15:47:09 +0100

    rm xcode installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,11 @@ echo "==> Running bootstrap.sh"
 echo "Updating OSX..."
 sudo softwareupdate --install --all --verbose
 
-echo "Installing Xcode command line tools..."
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
-CMD_LINE_TOOLS=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
-sudo softwareupdate -i "$CMD_LINE_TOOLS" --verbose
-rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+# echo "Installing Xcode command line tools..."
+# touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+# CMD_LINE_TOOLS=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
+# sudo softwareupdate -i "$CMD_LINE_TOOLS" --verbose
+# rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 # xcode-select --install
 
 echo "Cloning dotfiles repo..."
